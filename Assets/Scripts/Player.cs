7963f7142ff60c9e8f8d16216b28cdb6b317af40
lior1204 <lior12042000@gmail.com>
2022-05-13 12:31:23 +0300

    ruin movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
 
     [SerializeField] private float rotateSpeed = 0.5f;
     [SerializeField][Range (0.005f,0.05f)] private float rotationAcceleration = 0.01f;
+    [SerializeField] private float tiltSpeed = 0.01f;
+    [SerializeField] private float tiltMax = 30f;
 
     [SerializeField] private float boostChargeClock = 1.3f;
     [SerializeField] private float boostCooldown = 5f;
@@ -22,17 +24,21 @@ public class Player : MonoBehaviour
     [SerializeField] private float boostActiveTime= 2f;
 
     [SerializeField] private float projectileSpeed = 20f;
-
+    [SerializeField] private float shootDelay = 1.3f;
 
     //references
     [Space(2)][Header("References")]
     [SerializeField] Projectile _projectilePrefab;
     private Rigidbody rb;
+    private Camera _camera;
 
     //state
     private Vector2 movementInput;
     private float currentRotation = 0;
     private float targetRotation = 0;
+
+    private float tiltInput = 0;
+    private float tilt = 0;
 
     private float currentSpeed = 0;
     private float targetSpeed = 0;
@@ -42,10 +48,16 @@ public class Player : MonoBehaviour
     private bool isBoost = false;
     private float boostCooldownTimer = 0;
 
+    private bool isShooting=false;
+    Coroutine shootingCoroutine;
 
+    //raycast
+    private float distance = 100f;
+    [SerializeField] private LayerMask layer;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        _camera = Camera.main;
     }
     void Update()
     {
@@ -53,18 +65,42 @@ public class Player : MonoBehaviour
         ChargeBoost();
     }
 
-    
+    private void InputMouse()
+    {
+        Ray ray = _camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if(Physics.Raycast(ray, out RaycastHit hit, distance, layer)){
+
+        }
+    }
 
     private void Movement()
     {
+        tiltInput = 0;
         //accelerate rotation
         if (movementInput.x != 0)
         {
             targetRotation = rotateSpeed * Mathf.Sign(movementInput.x);
+            tiltInput = -movementInput.x;
         }
         else targetRotation = 0;
         currentRotation =Mathf.Lerp(currentRotation,targetRotation, rotationAcceleration);
-        transform.Rotate(0,currentRotation, 0);//rotate
+        float currTilt = transform.rotation.eulerAngles.z;
+        if (tiltInput!=0)
+        {
+            if (currTilt < tiltMax || currTilt > 360 - tiltMax)
+                tilt = tiltSpeed * tiltInput;
+            else
+                tilt = 0;
+        }
+        else if(currTilt <= 180)
+        {
+            tilt = -tiltSpeed;
+        }
+        else
+        {
+            tilt = tiltSpeed;
+        }
+        transform.Rotate(0,currentRotation, tilt);//rotate
 
         //accelerate speed
         if (isBoost)
@@ -111,13 +147,27 @@ public class Player : MonoBehaviour
     {
         boostPressed = context.phase == InputActionPhase.Performed;
     }
+
     public void ShootProjectile(InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Performed)
         {
-            Projectile p = Projectile.Shoot(_projectilePrefab, transform.forward, projectileSpeed);
-            p.transform.position = transform.position;
+            isShooting = true;
+            if(shootingCoroutine==null)shootingCoroutine=StartCoroutine(Shoot());
+        }
+        if (context.phase == InputActionPhase.Canceled)
+        {
+            isShooting = false;
+            shootingCoroutine = null;
         }
     }
-
+    private IEnumerator Shoot()
+    {
+        while (isShooting)
+        {
+            Projectile p = Projectile.Shoot(_projectilePrefab, transform.forward, projectileSpeed);
+            p.transform.position = transform.position;
+            yield return new WaitForSeconds(shootDelay);
+        }
+    }
 }
